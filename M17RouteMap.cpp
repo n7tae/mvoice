3257a2b592e95f3d4db80d0ea8bd53b5de09aeaf
Tom Early <n7tae@tearly.net>
2021-01-16 16:27:06 -0700

    copyright

--- M17RouteMap.cpp
+++ M17RouteMap.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2020 by Thomas A. Early N7TAE
+ *   Copyright (c) 2020-2021 by Thomas A. Early N7TAE
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
